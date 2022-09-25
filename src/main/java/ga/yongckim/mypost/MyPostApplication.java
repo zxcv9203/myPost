1b502438eeff2ea064373fa638d407f87fc243bd
yongckim <zxcv9203@naver.com>
2022-09-25 20:49:02 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/ga/yongckim/mypost/MyPostApplication.java
@@ -0,0 +1,13 @@
+package ga.yongckim.mypost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ga/yongckim/mypost/MyPostApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.yongckim.mypost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
